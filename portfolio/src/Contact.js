311b933f477347a9a18dd21be86e11b826329b29
leenapatil14 <57280271+leenapatil14@users.noreply.github.com>
2020-08-11 00:14:57 -0400

    leena contact us

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -3,6 +3,10 @@ import { withStyles } from '@material-ui/core/styles';
 import { Container } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import MailIcon from '@material-ui/icons/Mail';
 const useStyles = theme => ({
     root: {
         flexGrow: 1,
@@ -40,16 +44,63 @@ const useStyles = theme => ({
     },
     containerSpacing: {
         margin: 500
-    }
+    },
+    skillsLabel:{
+        textAlign: 'center',
+          color:'#a50369',
+          fontWeight:600
+      },
+      containerTabs:{
+          margin:20
+      },
+      contactSection:{
+        color:'#8E8E89',
+        fontSize:12,
+        textAlign:'center',
+        fontWeight:400,
+        marginBottom:21
+    },
 
 
 
 });
 export default withStyles(useStyles)(class Contact extends Component {
     render() {
+        const {classes} = this.props;
         return (
             <Container>
-                hiii
+                <div className={classes.containerTabs}>
+                    <Grid container direction="row"
+                        justify="center"
+                        alignItems="center" spacing={3} >
+                        <Grid item xs={12} className={classes.skillsLabel}>
+                            <Link href="https://www.linkedin.com/in/adityasonawane/" >
+
+                                <div><LinkedInIcon fontSize="large" /></div>
+                                <div className={classes.contactSection}>Connect on LinkedIn!</div>
+
+                            </Link>
+                        </Grid>
+                        <Grid item xs={12} className={classes.skillsLabel}>
+                            <Link href="https://github.com/adisonawane" >
+
+                                <div><GitHubIcon fontSize="large" /></div>
+                                <div className={classes.contactSection}>Checkout my projects on Github</div>
+                            </Link>
+                        </Grid>
+                        <Grid item xs={12} className={classes.skillsLabel}>
+                            <Link href="mailto:adityasonawane0194@gmail.com?" >
+
+                                <div><MailIcon fontSize="large" /></div>
+                                <div className={classes.contactSection}>Have  queries? Feel free to mail!</div>
+                            </Link>
+                        </Grid>
+
+                    </Grid>
+
+
+
+                </div>
             </Container>
         )
     }
